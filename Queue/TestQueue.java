e5c0f6b6c4463ad363a62fd4edefd009ccd93d22
oscarhdezs <36929495+oscarhdezs@users.noreply.github.com>
2018-11-23 18:06:46 -0600

    Create TestQueue.java

---
+++ Queue/TestQueue.java
@@ -0,0 +1,19 @@
+public class TestQueue {
+
+	public static void main(String[] args) {
+		Queue queue = new Queue(1000);
+
+		queue.enqueue(10);
+		queue.enqueue(20);
+		queue.enqueue(30);
+		queue.enqueue(40);
+
+		System.out.println(queue.dequeue() + " dequeued from queue\n");
+		System.out.println(queue.dequeue() + " dequeued from queue\n");
+
+		System.out.println("Front item is " + queue.getFront());
+
+		System.out.println("Rear item is " + queue.getRear());
+	}
+
+}
