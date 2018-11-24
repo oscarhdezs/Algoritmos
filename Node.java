dc72f91fa97f7a7dcc9d3fb32920ba5f70e5f14a
oscarhdezs <36929495+oscarhdezs@users.noreply.github.com>
2018-11-23 18:04:18 -0600

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,23 @@
+public class Node {
+
+	private Node next;
+	private int element;
+	
+	public Node(int element) {
+		this.element = element;
+	}
+	
+	public Node getNext() {
+		return next;
+	}
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	public int getElement() {
+		return element;
+	}
+	public void setElement(int element) {
+		this.element = element;
+	}
+	
+}
